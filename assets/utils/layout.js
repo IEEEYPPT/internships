4a622c438d342137bec76fa7dcff4e9c5f848b01
Ines Coelho <ines.opcoelho@gmail.com>
2016-04-10 20:43:45 +0100

    Edit profile page upon registry.

---
+++ assets/utils/layout.js
@@ -0,0 +1,147 @@
+/**
+ * Created by user on 10/04/2016.
+ */
+
+function initLayout ()
+{
+    addNavbar();
+    addUserInformation();
+}
+
+function addNavbar()
+{
+    var user = checkUserAuthentication();
+
+    if (user == 0)
+        $('#navbar-main').append(
+        "<div class='navbar navbar-inverse navbar-fixed-top'>" +
+            "<div class='container'>" +
+                "<div class='navbar-collapse collapse'>" +
+                    "<ul class='nav navbar-nav'>" +
+                        "<li><a href='http://yp.ieee-pt.org/'>IEEE Young Professionals Portugal</a></li>" +
+                    "</ul>" +
+                "</div><!--/.nav-collapse -->" +
+            "</div>" +
+        "</div>"
+        );
+    else
+        $('#navbar-main').append(
+            "<div class='navbar navbar-inverse navbar-fixed-top'>" +
+                "<div class='container'>" +
+                    "<div class='navbar-collapse collapse'>" +
+                        "<ul class='nav navbar-nav'>" +
+                            "<li><a href='http://yp.ieee-pt.org/'>IEEE Young Professionals Portugal</a></li>" +
+                        "</ul>" +
+                        "<ul class='nav navbar-nav navbar-right'>" +
+                            "<li> <a href='index.html'> About</a></li>" +
+                            "<li> <a href='#listStudents'> Students</a></li>" +
+                            "<li> <a href='#listCompanies'> Companies</a></li>" +
+                            "<li> <a href='#listInternships'> Internships</a></li>" +
+                        "</ul>" +
+                    "</div><!--/.nav-collapse -->" +
+                "</div>" +
+            "</div>"
+        );
+}
+
+function addUserInformation () {
+    var user = checkUserAuthentication();
+
+    //clean DIV
+    $('#userInformation').empty();
+
+    //add proper information on DIV according with the type of user in the system
+    switch (user) {
+        case "0": //non-authenticated
+            $('#userInformation').append(
+                "<div class='spacer'>&nbsp;</div>" +
+                    "<a href='index.html' onclick='addSigninForm();return false;'>Sign in</a> ·  " +
+                    "<a href='index.html' onclick='addRegisterForm();return false;'>Register</a>." +
+                "<div class='spacer'>&nbsp;</div>"
+            );
+            break;
+        case "1": //student
+            $('#userInformation').append(
+                "<div class='spacer'>&nbsp;</div>" +
+                    "Signed as Student <a href='userInfo'>user</a>  ·  " +
+                    "<a href='index.html' onclick='signOut()'>Sign out</a>" +
+                "<div class='spacer'>&nbsp;</div>"
+            );
+            break;
+        case "2": //company
+            $('#userInformation').append(
+                "<div class='spacer'>&nbsp;</div>" +
+                    "Signed as Company <a href='userInfo'>user</a>  ·  " +
+                "   <a href='index.html' onclick='signOut()'>Sign out</a>-->" +
+                "<div class='spacer'>&nbsp;</div>"
+            );
+            break;
+
+    }
+}
+
+function loadAboutInformation () {
+    $('#title').append(
+        spacer + "<h2>" + aboutTitle + "<\h2>"+ spacer
+    );
+
+    $('#container').append(
+        spacer + aboutText + spacer
+    );
+}
+
+function addSigninForm() {
+    //clean DIVs
+    $('#title').empty();
+    $('#container').empty();
+
+    //add Title
+    $('#title').append(
+        spacer + "<h2>" + signinTitle + "<\h2>"+ spacer
+    );
+
+    //add info
+    $('#container').append(
+        "<form>" +
+            "<div class='form-group'>" +
+                "<label for='inputEmail1'>IEEE Email address</label>" +
+                "<input type='email' class='form-control' id='inputEmail1' placeholder='Email'>" +
+            "</div>" +
+            "<div class='form-group'>" +
+                "<label for='inputPassword1'>Password</label>" +
+                "<input type='password' class='form-control' id='inputPassword1' placeholder='Password'>" +
+            "</div>" +
+            "<button type='submit' class='btn btn-default' onclick='signIn();return false;'>OK</button>" +
+        "</form>"
+    );
+}
+
+function addRegisterForm() {
+    //clean DIVs
+    $('#title').empty();
+    $('#container').empty();
+
+    //add Title
+    $('#title').append(
+        spacer + "<h2>" + registerTitle + "<\h2>"+ spacer
+    );
+
+    //add info
+    $('#container').append(
+        "<h4>" + registerText + "<\h4>"+ spacer
+    );
+
+    $('#container').append(
+        "<form>" +
+            "<div class='form-group'>" +
+                "<label for='inputEmail1'>IEEE Email address</label>" +
+                "<input type='email' class='form-control' id='inputEmail1' placeholder='Email'>" +
+            "</div>" +
+            "<div class='form-group'>" +
+                "<label for='inputPassword1'>Password</label>" +
+                "<input type='password' class='form-control' id='inputPassword1' placeholder='Password'>" +
+            "</div>" +
+            "<button type='submit' class='btn btn-default' onclick='register();return false;'>OK</button>" +
+        "</form>"
+    );
+}
