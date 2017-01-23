f9dac4e50a61bc512de6a4e6e2db507395878e0a
Diogo Eiras <eiras@outlook.com>
2017-01-23 21:25:34 +0000

    Missing cases

--- config/folders/setup.js
+++ config/folders/setup.js
@@ -22,6 +22,14 @@ module.exports = function(dirname) {
                         }
                     }
                 });
+            } else {
+                fs.mkdir(Path.join(dirname,'/public/upload/company/profile'),function(err){
+                    if(err){
+                        if(err.errno !== 'EEXIST'){
+                            //TODO: Deal with unexpected errors
+                        }
+                    }
+                });
             }
         });
         fs.mkdir(Path.join(dirname,'/public/upload/student'),function(err){
@@ -29,6 +37,14 @@ module.exports = function(dirname) {
                 if(err.errno !== 'EEXIST'){
                     //TODO: Deal with unexpected errors
                 }
+                fs.mkdir(Path.join(dirname,'/public/upload/student/profile'),function(err){
+                    if(err){
+                        if(err.errno !== 'EEXIST'){
+                            //TODO: Deal with unexpected errors
+                        }
+                    }
+                });
+            } else {
                 fs.mkdir(Path.join(dirname,'/public/upload/student/profile'),function(err){
                     if(err){
                         if(err.errno !== 'EEXIST'){
